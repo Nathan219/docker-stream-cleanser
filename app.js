0626f26d7f5218a4db7773d58b2a77d3b52156fb
Nathan219 <Nathan219@gmail.com>
2014-09-22 11:12:15 -0700

    Adding stream attachment

--- app.js
+++ app.js
@@ -19,3 +19,20 @@ module.exports = function(data) {
   }
   return result;
 };
+
+module.exports.cleanStreams = function (buildStream, clientStream) {
+  buildStream.on('data', function(data) {
+    var header = null;
+    if (!data || data.length < 8 || data[1] !== 0) {
+      clientStream.write(data.toString());
+    } else {
+      for (var pointer = 0; pointer < data.length;) {
+        header = data.slice(pointer, pointer += 8);
+        var size = header.readUInt32BE(4);
+        var payload = data.slice(pointer, pointer += size);
+        if (payload === null) break;
+        clientStream.write(payload.toString());
+      }
+    }
+  });
+};
