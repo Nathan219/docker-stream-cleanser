fed131bbf63e640fc17aa3bca73469f39d811b0c
Nathan219 <Nathan219@gmail.com>
2014-09-25 11:16:36 -0700

    Adding carriage return fix that is required for termJs

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-
-// Right now, we're just going to strip the headers that Docker sends.  If we ever want a way
-// to color certain input, don't chop off the first byte
-// 8 (unicode) bytes at the beginning of each output
-
-module.exports = function(data, encoding) {
+module.exports = function(data, encoding, fixCarriageReturns) {
   if (!Buffer.isBuffer(data)) {
     data = new Buffer(data, encoding);
   }
@@ -18,12 +13,23 @@ module.exports = function(data, encoding) {
     var size = header.readUInt32BE(4);
     var payload = data.slice(pointer, pointer += size);
     if (payload === null) break;
+    if (fixCarriageReturns) {
+      payload = payload.replace(/\r?\n/g, '\r\n');
+    }
     result += payload.toString();
   }
   return result;
 };
 
-module.exports.cleanStreams = function (buildStream, clientStream, encoding) {
+/**
+ * This takes data from an input stream, strips out the payload, then writes that payload
+ * to the output stream
+ * @param buildStream input stream
+ * @param clientStream output stream
+ * @param encoding encoding of the stream
+ * @param fixCarriageReturns replaces all of the extra carriage returns with only one
+ */
+module.exports.cleanStreams = function (buildStream, clientStream, encoding, fixCarriageReturns) {
   buildStream.on('data', function(data) {
     if (!Buffer.isBuffer(data)) {
       data = new Buffer(data, encoding);
@@ -40,6 +46,9 @@ module.exports.cleanStreams = function (buildStream, clientStream, encoding) {
         var size = header.readUInt32BE(4);
         var payload = data.slice(pointer, pointer += size);
         if (payload === null) break;
+        if (fixCarriageReturns) {
+          payload = payload.replace(/\r?\n/g, '\r\n');
+        }
         clientStream.write(payload.toString());
       }
     }
